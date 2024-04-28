5e5baf8339a179abbbc5824e1854f4d875b07a90
sungchulyun <proattacker@naver.com>
2024-04-28 22:19:14 +0900

    주문 비즈니스 로직

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+
+    @Test
+    public void testSample() throws Exception{
+        Assertions.assertThat("The Lord of the Rings").isNotNull()
+                .startsWith("The")
+                .contains("Lord")
+                .endsWith("Rings");
+    }
+}
