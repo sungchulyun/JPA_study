c83e649c2248deb0f3656e6cf420b57e2ff53507
sungchulyun <proattacker@naver.com>
2024-05-04 20:10:42 +0900

    feat : 상품등록 페이지, controller 추가

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("items", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm bookForm){
+        //setter 사용을 지양하고 createItem 메소드를 만들어서 하는 것이 정석..
+        Book book = new Book();
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/";
+    }
+}
